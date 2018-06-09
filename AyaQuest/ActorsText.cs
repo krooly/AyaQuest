e127e42fb0f33c443930f38be81e6c01c1f0b5ca
korzanov <korzanov@orbit-a.com>
2018-06-10 02:43:26 +0300

    create project

---
+++ AyaQuest/ActorsText.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyaQuest
+{
+    public enum TextStatus
+    {
+        none,
+        bad,
+        good
+    }
+
+    public enum Actors
+    {
+        Aya,
+        Nobody,
+        Somebody,
+        Arost,
+        elph,
+        graph,
+        pharlan,
+        saver,
+        severein,
+        man
+    }
+
+    public class QText
+    {
+        public QText(Actors actor, TextStatus status, string text)
+        {
+            Note = text;
+            Status = status;
+            Actor = actor;
+        }
+        public TextStatus Status { get; }
+        public string Note { get; }
+        public QActor Actor { get; }
+    }
+
+    public class QActor
+    {
+        public QActor(Actors Actor)
+        {
+            this.Actor = Actor;
+        }
+
+        public Actors Actor { get; }
+
+        public string GetName()
+        {
+            switch (Actor)
+            {
+                case Actors.Aya:
+                    return "Ая";
+                case Actors.Arost:
+                    return "Аристократ";
+                case Actors.elph:
+                    return "Эльф";
+                case Actors.graph:
+                    return "Граф Вейланд";
+                case Actors.man:
+                    return "Мужчина";
+                case Actors.Nobody:
+                    return "Незнакомец";
+                case Actors.pharlan:
+                    return "Фарлан";
+                case Actors.saver:
+                    return "Хранитель";
+                case Actors.severein:
+                    return "севирейн";
+                case Actors.Somebody:
+                    return "Кто-то";
+                default:
+                    return "АКТЕР НЕ ВЫБРАН";
+            }
+        }
+
+        public override string ToString()
+        {
+            return GetName();
+        }
+
+        public static implicit operator QActor(Actors Actor)
+        {
+            return new QActor(Actor);
+        }
+    }
+
+    public static class ActorsText
+    {
+        static readonly QText[] Aya = new QText[]
+        {
+            //new QText(Actors.Aya, TextStatus.none, ""),
+            //new QText(Actors.Aya, TextStatus.none, ""),
+            //new QText(Actors.Aya, TextStatus.none, ""),
+            //new QText(Actors.Aya, TextStatus.none, ""),
+            //new QText(Actors.Aya, TextStatus.none, ""),
+            //...
+        };
+    }
+}
---
+++ AyaQuest/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace AyaQuest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelActor = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(343, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Кнопка1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(342, 220);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Действующее лицо:";
+            // 
+            // labelActor
+            // 
+            this.labelActor.AutoSize = true;
+            this.labelActor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelActor.Location = new System.Drawing.Point(129, 13);
+            this.labelActor.Name = "labelActor";
+            this.labelActor.Size = new System.Drawing.Size(60, 16);
+            this.labelActor.TabIndex = 3;
+            this.labelActor.Text = "ACTOR";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(12, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(343, 37);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Кнопка2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 361);
+            this.Controls.Add(this.labelActor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Жизнь Аи";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelActor;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
